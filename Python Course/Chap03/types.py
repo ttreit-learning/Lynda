4b230961ac52367960c273f6fc26c3b56248139a
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-29 13:07:36 -0700

    Practicing

--- Python Course/Chap03/types.py
+++ Python Course/Chap03/types.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+# x = 7
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = 'seven {} {}'.format(8, 9)
+# print('x is {}'.format(x))
+# print(type(x))
+
+# f strings - work like .format method
+
+# a = 8
+# b = 9
+# x = f'seven {a:>09} {b}'
+# print('x is {}'.format(x))
+# print(type(x))
+
+x = -1
 print('x is {}'.format(x))
 print(type(x))
+
+if x:
+    print('True')
+else:
+    print('False')
+
