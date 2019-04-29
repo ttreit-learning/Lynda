5b5531d6d800446cb53184a0366fb3c2a8874ca4
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-29 14:08:54 -0700

    Practice

--- Python Course/Chap04/conditional.py
+++ Python Course/Chap04/conditional.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-if True:
+## Original
+# if True:
+#     print('if true')
+# elif False:
+#     print('elif true')
+# else:
+#     print('neither true')
+## Original End
+
+
+if False:
     print('if true')
-elif False:
+elif True:
     print('elif true')
 else:
     print('neither true')
--- Python Course/Chap04/ternary.py
+++ Python Course/Chap04/ternary.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-hungry = True
+##Original
+# hungry = True
+# x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
+# print(x)
+## End Original
+
+
+hungry = 1
+# 1 can be used for true, 0 for false
 x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
 print(x)
