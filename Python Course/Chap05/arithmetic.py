a16cc4dd15a8e72656fa764715fd881d5a879f66
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-30 12:58:33 -0700

    Practicing

--- Python Course/Chap05/arithmetic.py
+++ Python Course/Chap05/arithmetic.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Original
+# x = 5
+# y = 3
+# z = x + y
+
+# print(f'result is {z}')
+# End Original
+
 x = 5
 y = 3
 z = x + y
--- Python Course/Chap05/boolean.py
+++ Python Course/Chap05/boolean.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# a = True
+# b = False
+# x = ( 'bear', 'bunny', 'tree', 'sky', 'rain' )
+# y = 'bear'
+
+# if a and b:
+#     print('expression is true')
+# else:
+#     print('expression is false')
+
 a = True
 b = False
 x = ( 'bear', 'bunny', 'tree', 'sky', 'rain' )
-y = 'bear'
+y = 'sky'
 
-if a and b:
+if y is not x[3]:
     print('expression is true')
 else:
     print('expression is false')
+
+print(id(y))
+print(id(x[0]))
