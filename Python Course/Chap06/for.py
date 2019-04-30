efe1c86b96d7a2d0ea5de0979dbe7339f3739dea
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-30 14:11:54 -0700

    Practicing

--- Python Course/Chap06/for.py
+++ Python Course/Chap06/for.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+# for pet in animals:
+#     print(pet)
+
 animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
 
 for pet in animals:
--- Python Course/Chap06/while.py
+++ Python Course/Chap06/while.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# secret = 'swordfish'
+# pw = ''
+
+# while pw != secret:
+#     pw = input("What's the secret word? ")
+
+
 secret = 'swordfish'
 pw = ''
+auth = False
+count = 0
+max_attempt = 5
 
 while pw != secret:
-    pw = input("What's the secret word? ")
+    count += 1
+    if count > max_attempt:
+        break
+    
+    pw = input(f"{count}: What's the secret word? ")
+else: 
+    auth = True
+
+print('Authorized' if auth else "Get off my computer you fraud!")
+
+
