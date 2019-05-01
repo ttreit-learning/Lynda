57a91f5081119922f067939406f38816885471b6
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-01 14:59:39 -0700

    Practicing

--- Python Course/Chap07/hello.py
+++ Python Course/Chap07/hello.py
@@ -3,11 +3,22 @@
 
 # print('Hello, World.')
 
+# This works
+# def f1():
+#     print('This is f1')
 
-def f1():
-    print('This is f1')
+# x = f1
+# x()
 
-x = f1
+def f1(f):
+    def f2():
+        print('this is before the function call')
+        f()
+        print('this is after the function call')
+    return f2
 
+def f3():
+     print ('this is f3')
 
-
+x = f1(f3)
+x()
