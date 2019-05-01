064728db14e5b5eb5826c8e19116c3360d9cf88e
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-30 23:13:50 -0700

    Practicing

--- Python Course/Chap07/args.py
+++ Python Course/Chap07/args.py
@@ -2,7 +2,11 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    kitten('meow', 'grrr', 'purr')
+    # kitten('meow', 'grrr', 'raaar', 'purr', 'woof')
+    # kitten(*args)
+    # OR
+    x = ('meow', 'grrr', 'raaar', 'purr', 'woof')
+    kitten(*x)
 
 def kitten(*args):
     if len(args):
