807350797b030eb8d6bed5e838f1a421316bdaf0
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-30 23:06:24 -0700

    Practicing

--- Python Course/Chap07/function.py
+++ Python Course/Chap07/function.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def main():
-    kitten()
+# def main():
+#     kitten()
 
-def kitten():
+# def kitten():
+#     print('Meow.')
+
+# if __name__ == '__main__': main()
+
+# 7.2
+def main():
+    x = 5
+    print(id(x))
+    kitten(x)
+    print(f'in main: x is {x}')
+
+def kitten(n):
+    print(id(n))
+    n = 3
+    print(id(n))
     print('Meow.')
+    print(n)
 
 if __name__ == '__main__': main()
+
+# 7.1
+# def main():
+#     print("Hello World!")
+#     x = kitten()
+#     print(x)
+
+# def kitten():
+#     print('Meow.')
+#     return 'harro'
+
+
+# if __name__ == '__main__': main()
+
