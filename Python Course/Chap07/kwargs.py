88c8162b41800172207530e0e4c58a4b90cd3cac
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-30 23:22:56 -0700

    Practicing

--- Python Course/Chap07/kwargs.py
+++ Python Course/Chap07/kwargs.py
@@ -2,12 +2,16 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    kitten(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
+    # kitten(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
+    # OR
+    dix = dict(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
+    kitten(**dix)
 
 def kitten(**kwargs):
     if len(kwargs):
         for k in kwargs:
-            print('Kitten {} says {}'.format(k, kwargs[k]))
+            # print('Kitten {} says {}'.format(k, kwargs[k]))
+            print(f'Kitten {k} says {kwargs[k]}')
     else: print('Meow.')
 
 if __name__ == '__main__': main()
