b504d3993d4bd2a225838db2b2fb16b9b028b206
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-01 17:08:50 -0700

    Practice

--- Python Course/Chap08/dict.py
+++ Python Course/Chap08/dict.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# def main():
+#     animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
+#         'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+#     print_dict(animals)
+
+# def print_dict(o):
+#     for x in o: print(f'{x}: {o[x]}')
+
+# if __name__ == '__main__': main()
+
 def main():
-    animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
-        'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    # the dict form is easier to type than the alternate method below
+    animals = dict(kitten = 'meow', puppy = 'ruff!', lion = 'grrr', giraffe = 'I am a giraffe!', dragon = 'raaar')
+    # for k, v in animals.items():
+    #     print (f'{k}: {v}')
+
+
+    # animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
+    #     'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
     print_dict(animals)
 
 def print_dict(o):
-    for x in o: print(f'{x}: {o[x]}')
+    for k, v in o.items():
+        print (f'{k} {v}')
+    # for x in o: print(f'{x}: {o[x]}')
 
 if __name__ == '__main__': main()
+
