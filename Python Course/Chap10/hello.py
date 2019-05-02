75109a0f72a517fedee9e049b925a23c4ea6f6ab
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-02 10:19:19 -0700

    Practicing Exceptions

--- Python Course/Chap10/hello.py
+++ Python Course/Chap10/hello.py
@@ -2,6 +2,13 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    print('Hello, World.')
+    try:        
+        x = 5/0
+    except ValueError:
+        print('I caught a ValueError')
+    except ZeroDivisionError:
+        print('Can\'t divide by zero, son')
+    else:
+        print('Good job!')
 
 if __name__ == '__main__': main()
